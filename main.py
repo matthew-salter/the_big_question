a10d82788cf0a0770255df595dc8c862c98d4398
matthew-salter <msalter@panelitix.ai>
2025-06-27 13:19:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ BLOCKING_PROMPTS = {
     "read_supply_report",
     "read_demand_report",
     "write_prompt_1_elasticity",
-    "read_prompt_1_elasticity"
+    "read_prompt_1_elasticity",
+    "write_elasticity_maths"
 }
 
 PROMPT_MODULES = {
@@ -76,7 +77,8 @@ PROMPT_MODULES = {
     "read_supply_report": "Scripts.Elasticity.read_supply_report",
     "read_demand_report": "Scripts.Elasticity.read_demand_report",
     "write_prompt_1_elasticity": "Scripts.Elasticity.write_prompt_1_elasticity",
-    "read_prompt_1_elasticity": "Scripts.Elasticity.read_prompt_1_elasticity"
+    "read_prompt_1_elasticity": "Scripts.Elasticity.read_prompt_1_elasticity",
+    "write_elasticity_maths": "Scripts.Elasticity.write_elasticity_maths"
 }
 
 # --- ROUTES ---
